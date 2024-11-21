e91cb4481690cc67d463170b5ea7b2f7ade9992a
Kauã Freitas <kauapesssoadefreitas@gmail.com>
2024-11-21 15:41:18 -0300

    Parte inicial do projeto desenvolvida,

---
+++ kaua/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Evolução</title>
+</head>
+
+<body>
+
+    <nav class="navigation-bar">
+        <a href="../evolucao.php">Voltar para a página inicial</a>
+    </nav>
+
+    <main>
+
+        <section class="main-content">
+
+            <div class="projects-container">
+
+                <div class="project">
+                    <div class="title">
+                        <h3>Primeiro projeto</h3>
+                    </div>
+                    <div class="text">
+                        <p>Projeto sobre E-Commerce utilizando linguagem de marcação, de estilização e programação (HTML, CSS e PHP respectivamente)</p>
+                    </div>
+                    <div class="img-container">
+                        <img src="assets/first project img.png" alt="" height="350px">
+                    </div>
+                </div>
+
+                <div class="project">
+                    <div class="title">
+                        <h3>Segundo projeto</h3>
+                    </div>
+                    <div class="text">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, eos quasi. Laboriosam quo modi est enim illo harum sint qui dolorum sapiente odit quibusdam consectetur, adipisci cumque, inventore autem natus incidunt voluptate aperiam architecto reprehenderit ipsam. Corporis tempore atque, in inventore dolores expedita suscipit sint repellendus distinctio labore adipisci quod!</p>
+                    </div>
+                    <div class="img-container">
+                        <img src="assets/first project img.png" alt="" height="350px">
+                    </div>
+                </div>
+
+            </div>
+
+        </section>
+
+    </main>
+
+</body>
+
+</html>
