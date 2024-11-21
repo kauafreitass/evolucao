9d6f9c545d42dbfa685767ecf8eff0b3e37b13ab
Ana Luisa <analuisavasconcelos.sesisenai@gmail.com>
2024-11-21 11:47:49 -0300

    Save

---
+++ ana/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EVOLUCAO</title>
+</head>
+<body>
+
+<nav>
+    <label for="EVOLUÇÃO">EVOLUÇÃO</label>
+    <ul>
+        <li><a href="#">HOME</a></li>
+        <li><a href="#">PROJETO ANTIGO</a></li>
+        <li><a href="#">PROJETO ATUAL</a></li>
+        <li><a href="#">COMPARAÇÃO</a></li>
+    </ul>
+</nav>
+    
+</body>
+</html>
