37dd43e7e713bde32e8bec3f149c95ea5e61015d
Ana Luisa <analuisavasconcelos.sesisenai@gmail.com>
2024-11-25 08:30:36 -0300

    save

--- ana/evolucao.php
+++ ana/evolucao.php
@@ -1,13 +1,139 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Evolução</title>
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <script defer src="app.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css">
 </head>
-<body>
 
-    
-    
+<body>
+    <header>
+        <nav>
+            <a href="#" class="logo"> EVOLUÇÃO</a>
+
+            <button class="hamburger"></button>
+
+
+            <ul class="nav-list">
+                <li><a href="">HOME</a></li>
+                <li><a href="">PROJETO ANTIGO</a></li>
+                <li><a href="">COMPARAÇÕES</a></li>
+
+            </ul>
+        </nav>
+    </header><br><br>
+    <main class="contentainer">
+        <div class="contentbase">
+
+
+            <div class="textcontent">
+                <h1>TITULO AQUI</h1>
+
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit, culpa, magni veniam consequuntur quam
+                    nostrum illo eum nemo quidem facere tempore excepturi fuga! Adipisci architecto, hic quidem ducimus
+                    facere, necessitatibus eius eum dolore labore nostrum aliquid exercitationem placeat culpa dolor
+                    laborum dolores mollitia quod unde consequuntur praesentium asperiores iusto natus nihil iure. Dicta
+                    velit fugiat, quos illo suscipit nobis consequatur explicabo aut? Ab aspernatur accusantium ipsa
+                    minima, impedit ut eius similique assumenda odit consequatur harum deserunt autem quam quae omnis et
+                    minus, enim officiis optio molestias excepturi iusto vel? Labore voluptate magni omnis rerum maiores
+                    rem nulla repellendus dolores, ipsa veritatis reprehenderit incidunt odit obcaecati recusandae in.
+                    Non officia repellat, m modi neque saepe reiciendis, soluta fugiat
+                    aliquid adipisci recusandae voluptatum natus iure accusamus voluptates pariatur consectetur rem!
+                    Accusamus voluptatibus quam quos adipisci. Aperiam ducimus molestiae minus iure vero consectetur
+                    nisi quia non nobis illum!</p><br>
+                <button class="basestyle">SAIBA MAIS SOBRE O PROJETO ATUAL</button>
+
+            </div><br><br>
+            <div class="imgcontent">
+                <img src="Captura de tela 2024-07-19 033801.png" alt="" class="imgbase">
+            </div>
+        </div><br><br><br><br>
+
+
+        <div class="containerjustext">
+            <div class="contjustext"><br>
+                <h1>
+                    TITULO TOPICO
+                </h1><br><br>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga cum placeat cumque animi possimus
+                    obcaecati
+                    neque tempore. Dolor nam libero excepturi blanditiis ad doloribus, qui vero quisquam obcaecati,
+                    velit,
+                    corporis molestiae soluta consequatur et cumque aut dolore. Obcaecati sint laudantium tenetur dolor,
+                    aperiam dolorem pre iste earum, accusamus officia obcaecati blanditiis illum
+                    repellendus atque quos exercitationem delectus molestias provident dolorem recusandae. Eius
+                    accusamus
+                    blanditiis ducimus libero voluptatum aliquam, rerum repellendus suscipit similique possimus tempore
+                    expedita? Asperiores ex tenetur at incidunt, error odit exercitationem porro? Exercitationem, sed
+                    eius.
+                    Placeat consequatur, asperiores facilis inventore a atque aperiam! Doloribus delectus quos
+                    repudiandae
+                    sequi. Sequi vero atque, laudantium veniam a dolor excepturi nobis minus temporibus explicabo sit
+                    eos
+                    saepe, accusantium sunt quia consequatur hic quidem voluptatum repudiandae reprehenderit consectetur
+                    laboriosam at! Numquam hic distinctio facilis asperiores eos quos mollitia illum, quaerat reiciendis
+                    consequatur, necessitatibus facere eius qui quo ipsum eveniet cumque illo at? Culpa ipsa minus aut
+                    quos
+                    dolore quibusdam! Quo eligendi et qui libero, illo corporis doloribus vitae reprehenderit sint
+                    maxime
+                    enim quisquam illum explicabo? Quidem ipsum ea illum labore explicabo officia praesentium,
+                    dignissimos
+                    mollitia, quos placeat quis voluptatibus harum et quasi animi tempore provident sed sint rem,
+                    doloribus
+                    voluptates. Ex at ducimus et magnam?</p><br>
+            </div>
+        </div>
+
+
+        <div class="topicontainer">
+   
+
+            <div class="topic">
+                <div class="imgtopicontai">
+                    <img src="img/projetoantigo.png" alt="" class="imgtopic">
+                </div><br>
+
+                <div class="textcont">
+                    <h2>PROJETO ANTIGO</h2>
+                    <p>DESCRIÇÃO DO PROJETO ANTIGO</p><br>
+                    <a href=""><button> PROJETO ANTIGO</button></a>
+                </div><br>
+            </div>
+
+       
+            <div class="topic">
+                <div class="imgtopicontai">
+                    <img src="img/projetoatual.png" alt="" class="imgtopic">
+                </div><br>
+
+                <div class="textcont">
+                    <h2>PROJETO ATUAL</h2>
+                    <p>DESCRIÇÃO DO PROJETO ATUAL</p><br>
+                    <a href=""><button> PROJETO ATUAL</button></a>
+                    
+                </div><br>
+            </div>
+
+          
+            <div class="topic">
+                <div class="imgtopicontai">
+                    <img src="img/projetoantigo.png" alt="" class="imgtopic">
+                </div><br>
+
+                <div class="textcont">
+                    <h2>COMPARAÇÕES</h2>
+                    <p>VEJA MAIS SOBRE AMBOS</p><br>
+                    <a href=""><button>COMPARAÇÕES</button></a>
+                </div><br>
+            </div>
+
+    </main>
+
+    <script src="app.js"></script>
 </body>
+
 </html>
