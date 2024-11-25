ce69156e7725402ad5e60cb077ebf7a833ea4a35
Gustavo Bueno <gustavobueno.sesisenai@gmail.com>
2024-11-25 11:13:58 -0300

    comeco

---
+++ GustavoBueno/primeirotrabalho.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Evolução</title>
+</head>
+
+<body>
+    <div><img src="img/Captura de tela 2024-11-25 105149.png" alt="gg"></div>
+    <h1>meu primeiro site</h1>
+    <strong>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias sapiente temporibus, est veniam, expedita amet repudiandae odio tempora magnam eligendi saepe maiores cumque soluta possimus omnis voluptatem esse iste assumenda?</strong>
+</body>
+
+</html>
